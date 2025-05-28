88711c6e1de76930e58b0244a830d39077fea984
arthur <arthur.veigamuller@gmail.com>
2025-05-28 16:22:10 -0300

    update vala photos

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function toggleMode() {
   //substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    image.setAttribute("src", "./assets/vala-light.png") //Para substituir ou add imagem, setatrribute, serve para modificar um atributo em valor string('')
+    image.setAttribute("src", "./assets/vala.png") //Para substituir ou add imagem, setatrribute, serve para modificar um atributo em valor string('')
   } else {
     // se tiver sem light mode, manter a imagem normal
     image.setAttribute("src", "./assets/vala.png")
